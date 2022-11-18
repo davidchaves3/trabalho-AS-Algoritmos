471a0c519f60c38a1a448b9ab323440da9400ea1
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-11-17 23:57:39 -0300

    up

--- ex0001.py
+++ ex0001.py
@@ -9,31 +9,31 @@ gabarito = [] # lista que vai aguardar o nome da disciplina e o gabarito da prov
 classificados = [] #Lista para os alunos que tiram nota maior ou igual a 5
 desclassificados = [] #Lista para os alunos que tiram nota menor ou que 5
 
-print("=============================== GABARITO MODELO ===============================")
+print("============================= GABARITO MODELO =============================")
 # Cadastro do gabarito da Disciplina e o nome 
 while i < 11:
   if i == 1: #Condicinal para quando o contador for igual a 1 mostre a mensagem para adicional a disciplina
-    print("=================================================================================\n")
+    print("=================================================================================")
     gabarito.append(input("Digite o Nome da Disciplina: "))
-    print("==================================================================================\n")
+    print("=================================================================================")
   gabarito.append(input(f"Digite a resposta da QUESTÃO {i} ==> "))
   i = i + 1
 lista.append(gabarito)
-print("==================================================================================")
+print("=================================================================================")
 i = 0
 # Cadastro dos alunos e registro das respostas
 print("=================================== RESPOSTAS ===================================")
 while i < 30:
   e = 1 # Declarei o contador dentro do lado para que toda vez que repetir o contador volte ao valor inicial
   alunos = []
-  print("=================================================================================\n")
+  print("=================================================================================")
   alunos.append(input("Digite o NOME do ALUNO : "))
-  print("=================================================================================\n")
+  print("=================================================================================")
   #Laço de repetição para registrar a respostas da Prova 
   while e < 11:
     alunos.append(input(f"DIGITE A RESPOSTA DA QUESTÃO {e} ==> "))
     e = e + 1
-  print("==================================================================================\n")
+  print("=================================================================================")
   # Zerando o contador e a variável nota para que toda vez que iniciar o laço retorne ao valor 0
   nota = 0
   cont = 0
@@ -55,22 +55,22 @@ while i < 30:
 
 # Imprimindo o gabarito da prova e a resposta de cada ALUNO
 i = 0
-print("==================================================================================\n")
+print("==================================================================================")
 while i < len(lista):
   print(lista[i])
   i = i + 1
 
 # Imprimindo o NOME dos Alunos que foram CLASSIFICADOS
 i = 0
-print("================================ ALUNOS CLASSIFICADOS =================================")
+print("=============================== ALUNOS CLASSIFICADOS ===============================\n")
 while i < len(classificados):
   print(classificados[i])
   i = i + 1
 
 # Imprimindo o NOME dos Alunos que foram DESCLASSIFICADOS
 i = 0
-print("================================ ALUNOS DESCLASSIFICADOS ================================")
+print("=============================== ALUNOS DESCLASSIFICADOS ===============================\n")
 while i < len(desclassificados):
   print(desclassificados[i])
   i = i + 1
-print("==================================== FINALIZADO ==========================================")
+print("=================================== FINALIZADO ==========================================")
